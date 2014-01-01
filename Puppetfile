fd9ebf6c7091519971198a0cb125aa1455a36323
Gareth Rushgrove <gareth@morethanseven.net>
2014-01-01 15:05:23 +0000

    initial working commit

---
+++ Puppetfile
@@ -0,0 +1,7 @@
+forge "http://forge.puppetlabs.com"
+
+mod 'saz/locales'
+mod 'saz/motd'
+mod 'saz/timezone'
+
+mod 'puppetlabs/ntp'
---
+++ scripts/puppet.sh
@@ -0,0 +1,5 @@
+# Install Ruby
+sudo apt-get install ruby rubygems -y
+
+# Install puppet/facter
+sudo gem install puppet facter --no-ri --no-rdoc
