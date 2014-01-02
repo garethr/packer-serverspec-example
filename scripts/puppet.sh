62f48c795e54588994cc3f97f61056ca6cc06072
Gareth Rushgrove <gareth@morethanseven.net>
2014-01-02 17:54:02 +0000

    fixes for issue installing ntp package

--- scripts/puppet.sh
+++ scripts/puppet.sh
@@ -1,6 +1,12 @@
+#!/bin/bash
+
+# Install language locale as without can 
+# interfere with package installation
+sudo apt-get install language-pack-en -y
+
 # Install Ruby
 sudo apt-get update
-sudo apt-get install ruby1.9.1 rubygems1.9.1 -y
+sudo apt-get install ruby1.9.1 -y
 
 # Install puppet/facter
 sudo gem install puppet facter --no-ri --no-rdoc
