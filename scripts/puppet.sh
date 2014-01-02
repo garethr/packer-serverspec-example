f8522c62bd6613594dfe05fef59508e4fed75ed9
Gareth Rushgrove <gareth@morethanseven.net>
2014-01-02 10:44:22 +0000

    correct ruby package names (hopefully)

--- scripts/puppet.sh
+++ scripts/puppet.sh
@@ -1,6 +1,6 @@
 # Install Ruby
 sudo apt-get update
-sudo apt-get install ruby rubygems -y
+sudo apt-get install ruby1.9.1 rubygems1.9.1 -y
 
 # Install puppet/facter
 sudo gem install puppet facter --no-ri --no-rdoc
